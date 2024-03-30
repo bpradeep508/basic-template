564359df3c4172dcb03726473def21b76bec9b80
bpradeep508 <46367140+bpradeep508@users.noreply.github.com>
2024-03-30 16:39:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-
+import nufhe
 """
 # Welcome to Streamlit!
 
@@ -29,12 +29,22 @@ df = pd.DataFrame({
     "idx": indices,
     "rand": np.random.randn(num_points),
 })
+import random
+import nufhe
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+size = 32
+bits1 = [random.choice([False, True]) for i in range(size)]
+bits2 = [random.choice([False, True]) for i in range(size)]
+reference = [not (b1 and b2) for b1, b2 in zip(bits1, bits2)]
+
+ctx = nufhe.Context()
+secret_key, cloud_key = ctx.make_key_pair()
+
+ciphertext1 = ctx.encrypt(secret_key, bits1)
+ciphertext2 = ctx.encrypt(secret_key, bits2)
+
+vm = ctx.make_virtual_machine(cloud_key)
+result = vm.gate_nand(ciphertext1, ciphertext2)
+result_bits = ctx.decrypt(secret_key, result)
+
+st.write(result_bits)
